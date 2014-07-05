aaa6038cca8dc7358d6cc2374cb6d1cc901957fb
Daniel Husar <dano.husar@gmail.com>
2014-07-05 17:30:54 +0100

    update test titles

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var movieTitle = require('./index.js');
 var assert = require('assert');
 
-it('It should get the proper movie name', function () {
+it('It should get the proper movie title', function () {
     assert.equal(movieTitle('Einstein.And.Eddington.2008.DVDRip.XviD.avi'), 'Einstein And Eddington');
     assert.equal(movieTitle('Edge of Tomorrow (2014) 720pTS-2-DVD DD2.0 NL Subs NLU002'), 'Edge of Tomorrow');
     assert.equal(movieTitle('Maleficent (2014) DD2.0 NL Subs TS2DVD-NLU002'), 'Maleficent');
@@ -17,7 +17,7 @@ it('It should get the proper movie name', function () {
     assert.equal(movieTitle('arranged.xvid-reserved.avi'), 'arranged');
 });
 
-it('It should get the proper tv show name', function () {
+it('It should get the proper tv show title', function () {
     assert.equal(movieTitle('The.Flash.2014.S01E01.720p.WEB.x264-NOGRP[rartv]'), 'The Flash');
     assert.equal(movieTitle('Suits.S03E03.COMPLETE.720p.HDTV.x264-PublicHD'), 'Suits');
     assert.equal(movieTitle('24 S09E10 720p HDTV X264-DIMENSION [eztv]'), '24');
