cb9700f98ced4a3a2d4a8d35669e8679766754df
Daniel Husar <dano.husar@gmail.com>
2014-07-05 17:32:39 +0100

    regexp update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 module.exports = function (movieString) {
 
   var baseName = movieString.replace(/\.(avi|mkv|mpeg|mpg|mov|mp4)/i, '');
-  var movie = baseName.match(/(.*?)(dvdrip|xvid| cd[0-9]|dvdscr|brrip|divx|S[0-9]{1,3}E[0-9]{1,3}|Season[\s,0-9]{1,4}|[\{\(\[]?[0-9]{4}).*/i);
+  var movie = baseName.match(/(.*?)(dvdrip|xvid|cd[0-9]|dvdscr|brrip|divx|S[0-9]{1,3}E[0-9]{1,3}|Season[\s,0-9]{1,4}|[\{\(\[]?[0-9]{4}).*/i);
 
   if (movie && movie[1]) {
     movie = movie[1].replace(/\./g, ' ').trim();
